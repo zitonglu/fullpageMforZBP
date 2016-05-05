0ce592baa32737005f457365c42ef526bd240d7c
紫铜炉 <910109610@qq.com>
2016-05-05 16:38:23 +0800

    index.php:commets

--- template/index.php
+++ template/index.php
@@ -15,21 +15,26 @@
 					<p class="blogText1">{$subname}</p>
 					<div class="content1box">
 					<div class="content1">
+					{$lef=13}
+					<form class="form-horizontal more-margin" id="frmSumbit" target="_self" method="post" action="{$lef.CommentPostUrl}">
+					<input type="hidden" name="inpId" id="inpId" value="{$article.ID}" />
+					<input type="hidden" name="inpRevID" id="inpRevID" value="0" />
 						<div class="content1-list">
 							<label for="inpName" class="control-label-box">评论者(*)</label>
-							<input type="text" name="inpName" id="inpName" placeholder="您的称呼" tabindex="1" class="control-input-box">
+							<input type="text" name="inpName" id="inpName" placeholder="您的称呼" tabindex="1" class="control-input-box" value="{$user.Name}">
 						</div>
 						<div class="content1-list">
 							<label for="inpEmail" class="control-label-box">E-mail(*)</label>
-							<input type="text" name="inpEmail" id="inpEmail" placeholder="@"  tabindex="2" class="control-input-box">
+							<input type="text" name="inpEmail" id="inpEmail" placeholder="@"  tabindex="2" class="control-input-box" value="{$user.Email}">
 						</div>
 						<div class="content1-list">
 							<textarea name="txaArticle" id="txaArticle" rows="3" tabindex="3" class="form-control-box"></textarea>
 						</div>
 						<div class="content1-list">
-							<input type="text" name="inpHomePage" id="inpHomePage" placeholder="http://"  tabindex="4" class="control-input-box">
-							<button class="control-button-box">留言</button>
+							<input type="text" name="inpHomePage" id="inpHomePage" placeholder="http://"  tabindex="4" class="control-input-box" value="{$user.HomePage}">
+							<button class="control-button-box" onclick="return VerifyMessage()">留言</button>
 						</div>
+					</form>				
 					</div><!-- 未完成 -->
 					</div>
 				</div>
@@ -40,17 +45,17 @@
 			</div>
 		</div>
 		<div class="section">
-		{foreach $articles as $article}
-			{template:post-multi}
-		{/foreach}
-		<div class="slide">
-			<div class="pagination-box">
-				{template:pagebar}
+			{foreach $articles as $article}
+				{template:post-multi}
+			{/foreach}
+			<div class="slide">
+				<div class="pagination-box">
+					{template:pagebar}
+				</div>
 			</div>
 		</div>
+		<div class="section">
+
 		</div>
-<!-- 		<div class="section">
-			$article->id="9999999"
-		</div> -->
 	</div>
 {template:footer}
